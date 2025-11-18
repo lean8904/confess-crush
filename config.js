ef5cf25d9f101491468193793e3a2671f738446d
lean8904 <lethian8904@gmail.com>
2025-11-18 19:39:14 +0700

    Update CONFIG object with new values

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    titleWeb: "Iu nhao đeeeeeeeeeeeeeeee",
+    introTitle: 'Đạt Nghiêm',
+    introDesc: `OKEEE DÔ`,
+    btnIntro: '^^HeHe^^',
+    title: 'Chúc anh 19/11 vui vẻ 😘',
+    desc: 'Yeu em yeu em yeu em? ',
+    btnYes: 'Iuuuuu nhứmmmmmmm <33',
+    btnNo: 'Hem iu :3',
+    question: 'Em già em xấu yeu em hem?',
+    btnReply: 'Em yêu anh nhìu nhứm',
+    reply: 'Yêu thì yêu mà không yêu thì buộc yêu <33333333',
+    mess: 'Về anh em mình hẹn nhau bữa nhậu đê',
+    messDesc: 'Mái nà anh iem.',
+    btnAccept: 'Qúa oke em luôn <3',
+    messLink: 'https://www.facebook.com/Lean080904#' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
